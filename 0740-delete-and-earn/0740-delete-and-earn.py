9c2ba8b94088ef4d4ff4ad40d551ccc534de70bf
Harshit Raj <harshit2271822@gmail.com>
2026-08-04 21:08:55 +0530

    Time: 8 ms (44.76%), Space: 21.7 MB (23.07%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -1,23 +1,24 @@
+
+def f(idx, points, mx, memo):
+    if idx > mx:
+        return 0
+    if idx in memo:
+        return memo[idx]
+
+    take = points[idx] + f(idx + 2, points, mx, memo)
+    skip = f(idx + 1, points, mx, memo)
+
+    memo[idx] = max(take, skip)
+    return memo[idx]
+
+
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
+        mx = max(nums)
 
-        max_num = max(nums)
-        points = [0] * (max_num + 1)
-
-        
+        points = [0] * (mx + 1)
         for num in nums:
             points[num] += num
 
-        
-        prev_two = 0
-        prev_one = 0
-
-        for total_point in points:
-            current = max(prev_one, prev_two + total_point)
-            prev_two = prev_one
-            prev_one = current
-
-        return prev_one
-        
+        memo = {}
+        return f(0, points, mx, memo)
